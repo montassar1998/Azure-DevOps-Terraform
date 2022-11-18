8abaaeebb05d91fe321da3270fc900b2a3581f54
unknown <montassar1998>
2022-11-18 10:18:52 +0100

    form container group resource

--- main.tf
+++ main.tf
@@ -1,47 +1,40 @@
-provider "azurerm" {
-    version = "2.5.0"
-    features {}
-}
-
 terraform {
-    backend "azurerm" {
-        resource_group_name  = "tf_rg_blobstore"
-        storage_account_name = "tfstoragebinarythistle"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.92.0"
     }
+  }
 }
 
-variable "imagebuild" {
-  type        = string
-  description = "Latest Image Build"
+provider "azurerm" {
+  features {}
 }
+
 
 
 
 resource "azurerm_resource_group" "tf_test" {
-  name = "tfmainrg"
-  location = "Australia East"
+  name     = "tfmainrg"
+  location = "Qatar Central"
 }
 
+
 resource "azurerm_container_group" "tfcg_test" {
-  name                      = "weatherapi"
-  location                  = azurerm_resource_group.tf_test.location
-  resource_group_name       = azurerm_resource_group.tf_test.name
-
+  name                = "weatherapi"
+  location            = azurerm_resource_group.tf_test.location
+  resource_group_name = azurerm_resource_group.tf_test.name
   ip_address_type     = "public"
-  dns_name_label      = "binarythistlewa"
+  dns_name_label      = "weatherwa"
   os_type             = "Linux"
-
   container {
-      name            = "weatherapi"
-      image           = "binarythistle/weatherapi:${var.imagebuild}"
-        cpu             = "1"
-        memory          = "1"
+    name   = "weatehrapi"
+    cpu    = "1"
+    memory = "1"
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
 
-        ports {
-            port        = 80
-            protocol    = "TCP"
-        }
   }
 }
